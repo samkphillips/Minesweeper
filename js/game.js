a9d13628c5cfe05e995722a142372e6332409338
Sam <sam.k.phillips@gmail.com>
2021-08-09 17:36:50 -0400

    Some more barebones stuff. Have a grid and cells that know who they are. It is coming along

--- js/game.js
+++ js/game.js
@@ -1,17 +1,6 @@
-console.log('JS is up')
-
-// const testButton = document.querySelector('.test-button')
-
-// testButton.addEventListener('click', () => {
-//   console.log('Left click')
-// })
-
-// testButton.addEventListener('contextmenu', (e) => {
-//   e.preventDefault()
-//   console.log('Right click')
-// })
-
+//
 const BODY = document.querySelector('body')
+const GRIDCONTAINER = document.querySelector('.grid-container')
 
 class Cell {
   //fields:
@@ -22,31 +11,32 @@ class Cell {
   -location: object with two fields: {x: x coord, y: y coord}
   --so the x coord would be referenced with like, cell.location.x
   */
-  constructor(id) {
+  constructor(id, xCoord, yCoord) {
     this.id = id
 
-    this.timesClicked = 0
-
     this.cellElement = document.createElement('div')
+    this.cellElement.className = 'cell'
 
     this.cellElement.innerHTML = `<p>${this.id}</p>`
 
+    this.location = { x: xCoord, y: yCoord }
+
     //left click listener
     this.cellElement.addEventListener('click', (e) => {
-      console.log(`My id is ${this.id}`)
-      this.timesClicked++
-      this.cellElement.innerHTML = `<p>${this.id} has been clicked: ${this.timesClicked}</p>`
+      console.log(
+        `My id is ${this.id} and I am at ${this.location.x}, ${this.location.y}`
+      )
+      this.cellElement.className = 'cell a'
     })
 
     //right click listener
     this.cellElement.addEventListener('contextmenu', (e) => {
       e.preventDefault()
-      console.log(`My id is ${this.id}`)
-      this.timesClicked--
-      this.cellElement.innerHTML = `<p>${this.id} has been clicked: ${this.timesClicked}</p>`
+      console.log(
+        `My id is ${this.id} and I am at ${this.location.x}, ${this.location.y}`
+      )
+      this.cellElement.className = 'cell b'
     })
-
-    BODY.appendChild(this.cellElement)
   }
 
   isMine() {
@@ -75,7 +65,19 @@ class Grid {
   constructor() {
     //generate the board
     //fill it with cells
+
+    let cellCount = 0
+
+    for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 8; x++) {
+        let c = new Cell(cellCount, x, y)
+        GRIDCONTAINER.appendChild(c.cellElement)
+        cellCount++
+      }
+    }
   }
 }
 
-let testArr = [new Cell('A'), new Cell('B'), new Cell('C')]
+//let testArr = [new Cell('A'), new Cell('B'), new Cell('C')]
+
+let g = new Grid()
